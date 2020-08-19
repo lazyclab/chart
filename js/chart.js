506b4e3ba852a2ebdb8eae3faef4805132d3a811
Vahagn <boxgold014@gmail.com>
2020-08-19 15:12:12 +0400

    first commit

--- js/chart.js
+++ js/chart.js
@@ -1,124 +1,114 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    //  let $click1 = $('.chart__button-1')
+    //  let $click2 = $('.chart__button-2')
+    //  console.log(k);
+    let k = document.querySelector('.chart__button-1')
+    let valE = 0;
+    let valS = 0;
+    let input = 0;
+    let hoursM = [];
 
+    let inputs = $('.section__myinput')
+    for (let i = 0; i < inputs.length; i++) {
+        input = inputs[i];
+    }
 
-   let valE = 0;
-   let valS = 0;
-   let result;
-   let resultS = [];
-   let input = 0;
-   let hoursM = [];
+    anychart.onDocumentReady(function() {
+        function getData(color) {
+            // generate data
+            var data = [
+                []
+            ];
+            var dataWeek = ['Пн', 'Вр', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+            var dataHours = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'];
+            // $('button').on('mouseup', e => {
 
-   let inputs = $('.myInput')
-   for (let i = 0; i < inputs.length; i++) {
-      input = inputs[i];
-   }
+            //     if ($('button').data('x').includes('a'))
+            //         obj.fill = "#0066FF";
+            //     else obj.fill = "#DFE4EF";
+            // })
+            for (var x = 0; x < dataHours.length; x++) {
+                hours = dataHours[x]
+                hoursM.push(+hours.split(':')[0])
+                    //$myFunc(hours)
+                for (var y = 0; y < dataWeek.length; y++) {
+                    let week = dataWeek[y];
+                    var obj = {};
+                    obj.x = hours;
+                    obj.y = week;
+                    if (color !== '') {
+                        obj.fill = "#4c4c4c";
+                        // k.addEventListener('click', e => {
+                        //         obj.fill = "#0066FF";
+                        //         //   console.log(k);
+                        //     })
+                        //     //   obj.fill = "#0066FF";
+                    }
+                    data.push(obj);
 
-   anychart.onDocumentReady(function () {
-      function getData() {
-         // generate data
-         var data = [
-            []
-         ];
-         var dataWeek = ['Пн', 'Вр', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-         var dataHours = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'];
-         if (resultS.length > 1) dataHours = resultS;
-         for (var x = 0; x < dataHours.length; x++) {
-            hours = dataHours[x]
-            hoursM.push(+hours.split(':')[0])
-            //$myFunc(hours)
-            for (var y = 0; y < dataWeek.length; y++) {
-               let week = dataWeek[y];
+                }
 
-               var obj = {};
-               obj.x = hours;
-               obj.y = week;
-
-               data.push(obj);
             }
-         }
-         return data;
-      }
-      // create a chart and set the data
-      var chart = anychart.heatMap(getData());
+            return data;
 
-      chart.yScroller().enabled(false);
-      chart.xScroller().enabled(false);
+        }
 
+        // create a chart and set the data
 
-      // disable labels
-      chart.labels(true);
+        var chart = anychart.heatMap(getData(''));
+        console.log(chart);
+        $('.chart__button-1').on('click', function() {
+            var chart = anychart.heatMap(getData('#c4c4c4'));
 
-      // set the chart title
-      chart.title("");
+        });
 
-      // set the container id
-      chart.container("container");
-
-      // initiate drawing the chart
-      chart.draw();
-      // enable and configure scrollers
-      chart.xZoom().setToPointsCount(8);
-
-      $(input).on('input', event => {
-         valS = +$(inputs[0]).val().split(':')[0]
-         valE = +$(inputs[1]).val().split(':')[0]
-         chart.xZoom().setToPointsCount(myFunc(valS, valE));
-         $myFunc(valS, valE)
-
-      })
+        chart.xScroller().enabled(true);
 
 
+        // disable labels
+        chart.labels(true);
 
-      function myFunc(a, b) {
-         if (a > b) {
+        // set the chart title
+        chart.title("");
 
-            return (a - b) + 1
-         }
+        // set the container id
+        chart.container("container");
 
-         else if (a < b) {
+        // initiate drawing the chart
+        chart.draw();
+        // enable and configure scrollers
+        console.log();
+        chart.xZoom().setToValues(valS, valE);
 
-            return (b - a) + 1
-         }
-         else
-            return 1;
-      }
+        $(input).on('input', event => {
+            valS = $(inputs[0]).val()
+            valE = $(inputs[1]).val()
+                // console.log(+$(this).val().split(':')[1]);
+                // // if (+$(this).val().split(':')[1] > 0)
+                // //     $(this).val().split(':')[1] = "00";
+            console.log(valS);
+            console.log(valE);
+            chart.xZoom().setToValues(valS, valE);
 
-   });
+
+        })
 
 
 
-   function $myFunc(a, b) {
+        //   function myFunc(a, b) {
+        //       if (a > b) {
 
-      if (a < b)
-         result = hoursM.filter(key => key >= a && key <= b);
-      else if (a > b)
-         result = hoursM.filter(key => key <= a && key >= b).reverse();
-      let k;
-      for (k of result) {
-         k = String(k);
-         if (k.length < 2) {
-            k = `0${k}`
-         }
-      }
+        //           return (a - b) + 1
+        //       } else if (a < b) {
 
-      resultS = result.map(item => {
-         return item
-      })
-      console.log(result);
-      return result;
-   }
+        //           return (b - a) + 1
+        //       } else
+        //           return 1;
+        //   }
+
+    });
+
+
+
 
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
